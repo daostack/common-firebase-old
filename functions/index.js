f0d6e23e6089f04cd06337a536f248f410fc73e5
lmcmz <lmcmze@gmail.com>
2020-05-14 18:46:24 +1000

    Uncomment Notification

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const ethers = require('ethers');
-// const Notification = require('./Notification')
+const Notification = require('./Notification')
 const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
