d3c8f7e8da0ffa327d2313857315d85c3a9e2aaf
Jelle <jellegerbrandy@gmail.com>
2020-05-18 16:59:11 +0200

    continue merge

--- functions/ArcListener.js
+++ functions/ArcListener.js
@@ -44,58 +44,7 @@ async function findUserByEthereumAddress(db, ethereumAddress) {
       return member.id
     }
 }
-async function updateProposals(first=null) {
-  // const response = []
-  const db = admin.firestore();
-  const proposals = await arc.proposals({first}).first()
-  console.log(`found ${proposals.length} proposals`)
-  
-  for (const proposal of proposals) {
-    const s = proposal.coreState
-    console.log(s)
 
-    // TODO: for optimization, consider looking for a new member not as part of this update process
-    // but as a separate cloudfunction instead (that watches for changes in the database and keeps it consistent)
-
-    // try to find the memberId corresponding to this address
-    const proposerId = await findUserByEthereumAddress(db, s.proposer)
-    let proposedMemberId
-    if (!s.proposedMember) {
-      proposedMemberId = null
-    } else if (s.proposer === s.proposedMember) {
-      proposedMemberId = proposerId
-    } else {
-      proposedMemberId = await findUserByEthereumAddress(db, s.proposedMember)
-    }
-      
-
-
-    const doc = {
-      title: s.title,
-      description: s.description,
-      createdAt: s.createdAt,
-      dao: s.dao.id,
-      name: s.name,
-      expiresInQueueAt: s.expiresInQueueAt,
-      proposer: s.proposer,
-      proposerId,
-      resolvedAt: s.resolvedAt,
-      stage: s.stage,
-      type: s.type,
-      joinAndQuit: {
-        proposedMemberAddress: s.proposedMember,
-        proposedMemberId: proposedMemberId,
-        funding: s.funding.toString()
-      },
-      votes: s.votes,
-      // TODO: votesFor and votesAgainst are in terms of reputation - we need to divide by 1000
-      votesFor: s.votesFor.toString(),
-      votesAgainst: s.votesAgainst.toString()
-    }
-    await db.collection('proposals').doc(proposal.id).set(doc)
-    return doc
-  }
-}
 // get all DAOs data from graphql and read it into the subgraph
 async function updateDaos() {
   const response = []
@@ -148,136 +97,72 @@ async function updateDaos() {
   return response.join('\n')
 }
 
-async function updatePlugins() {
 
+async function updateProposals(first=null) {
+  const db = admin.firestore();
+  const proposals = await arc.proposals({first}).first()
+  console.log(`found ${proposals.length} proposals`)
+  
+  for (const proposal of proposals) {
+    const s = proposal.coreState
+    console.log(s)
+
+    // TODO: for optimization, consider looking for a new member not as part of this update process
+    // but as a separate cloudfunction instead (that watches for changes in the database and keeps it consistent)
+
+    // try to find the memberId corresponding to this address
+    const proposerId = await findUserByEthereumAddress(db, s.proposer)
+    let proposedMemberId
+    if (!s.proposedMember) {
+      proposedMemberId = null
+    } else if (s.proposer === s.proposedMember) {
+      proposedMemberId = proposerId
+    } else {
+      proposedMemberId = await findUserByEthereumAddress(db, s.proposedMember)
+    }
+      
+
+
+    const doc = {
+      boostedAt: s.boostedAt,
+      description: s.description,
+      createdAt: s.createdAt,
+      dao: s.dao.id,
+      executionState: s.executionState,
+      executed: s.executed,
+      executedAt: s.executedAt,
+      expiresInQueueAt: s.expiresInQueueAt,
+      // TODO: votesFor and votesAgainst are in terms of reputation - we need to divide by 1000
+      votesFor: parseInt(s.votesFor),
+      votesAgainst: parseInt(s.votesAgainst),
+      id: s.id,
+      name: s.name,
+      preBoostedAt: s.preBoostedAt,
+      proposer: s.proposer,
+      proposerId,
+      resolvedAt: s.resolvedAt,
+      stage: s.stage,
+      title: s.title,
+      type: s.type,
+      joinAndQuit: {
+        proposedMemberAddress: s.proposedMember,
+        proposedMemberId: proposedMemberId,
+        funding: s.funding.toString()
+      },
+      votes: s.votes,
+      winningOutcome: s.winningOutcome,
+      // TODO: get actual links and images (these can be found in JSON.parse(s.description))
+      links: [{
+        title: "website",
+        url: s.url,
+      }],
+      images: [],
+    }
+
+    await db.collection('proposals').doc(proposal.id).set(doc)
+    return doc
+  }
 }
-
-async function updateProposals() {
-  arc.proposals({}, {subscribe: true, fetchAllData: true})
-    .subscribe(async proposals => {
-      proposals.map(async proposal => {
-        const {coreState} = proposal;
-        const proposalObject = {
-
-          boostedAt: coreState.boostedAt,
-          createdAt: coreState.createdAt,
-          dao: coreState.dao.id,
-          description: coreState.description,
-          expiresInQueueAt: coreState.expiresInQueueAt,
-          executionState: coreState.executionState,
-          executed: coreState.executed,
-          funding: coreState.funding.toString(),
-          executedAt: coreState.executedAt,
-          joinAndQuit: {
-            funding: coreState.funding.toString(),
-            proposedMemberId: coreState.proposer,
-            proposedMemberAddress: coreState.proposer,
-          },
-          preBoostedAt: coreState.preBoostedAt,
-          id: coreState.id,
-          name: coreState.name,
-          proposer: coreState.proposer,
-          proposerId: coreState.proposer,
-          resolvedAt: coreState.resolvedAt,
-          stage: coreState.stage,
-          title: coreState.title,
-          type: coreState.type,
-          votes: {
-            list: coreState.votes,
-            votesAgainst: parseInt(coreState.votesAgainst),
-            votesFor: parseInt(coreState.votesFor),
-          },
-          links: [{
-            title: "website",
-            url: coreState.url,
-          }],
-          images: [],
-          winningOutcome: coreState.winningOutcome,
-        }
-        await db.collection('proposals').doc(coreState.id).set(proposalObject)
-      });
-
-    })
-}
-
-// function updateDaosSubscription() {
-//   //loop that runs a function every 15 seconds for 3 intervals
-//   const response = ['xxx']
-//   try {
-//     const db = admin.firestore();
-//     arc
-//       .daos({}, {subscribe: true, fetchAllData: true})
-//       .subscribe(res => {
-//         res.map(async (dao, i) => {
-//           // const state = await dao.fetchState()
-//           // console.log(state)
-//           const {id, address ,
-//             memberCount,
-//             name,
-//             numberOfBoostedProposals ,
-//             numberOfPreBoostedProposals ,
-//             numberOfQueuedProposals ,
-//             register,
-//             reputation,
-//             reputationTotalSupply,
-//           } = dao.coreState;
-//           // if (!dao.coreState.name.includes('Test DAO') && !dao.coreState.name.includes('Car DAO')) {
-//           if (true) {
-//             const joinAndQuitPlugins = await dao.plugins({where: {name: 'JoinAndQuit'}}).first()
-//             if (joinAndQuitPlugins.length == 0) {
-//               // we'll delete this DAO, as it does not have the correct plugins
-//               try {
-//                 console.log('DELETING: ', id)
-//                 response.append(`Deleted dao ${id}`)
-//                 await db.collection('daos').doc(id).delete();
-//               } catch (e) {
-//                 console.error('Failed to updated DAOs: ', error);
-//               }
-//             } else {
-//               console.log('JaQ PLUGINS: ', joinAndQuitPlugins);
-//               const joinAndQuitPlugin = joinAndQuitPlugins[0]
-//               const {
-//                 fundingGoal,
-//                 minFeeToJoin,
-//                 memberReputation
-//               } = joinAndQuitPlugin.coreState.pluginParams;
-//               try {
-//                 await db.collection('daos').doc(id).set({
-//                   id,
-//                   address,
-//                   memberCount,
-//                   name,
-//                   numberOfBoostedProposals,
-//                   numberOfPreBoostedProposals,
-//                   numberOfQueuedProposals,
-//                   register,
-//                   reputationId: reputation.id,
-//                   reputationTotalSupply: parseInt(reputationTotalSupply),
-//                   fundingGoal: fundingGoal.toString(),
-//                   minFeeToJoin: minFeeToJoin.toString(),
-//                   memberReputation: memberReputation.toString()
-//                 })
-//                 console.log(`[ Updated DAO ${name}@${address}] `);
-//                 response.append(`updated DAO ${name}@${id}`)
-//               } catch (error) {
-//                 console.error(msg)
-//                 const msg = `Failed to updated DAOs: ${error}`;
-//                 throw Error(msg)
-//               }
-//             }
-//           }
-//           // if (dao.coreState.name.includes('Test DAO') || dao.coreState.name.includes('Car DAO')) {
-//           //   console.log('DELETING: ', id)
-//           //   await db.collection('daos').doc(id).delete();
-//           // }
-//         })
-//       });
-//   } catch(e) {
-//     console.log('Error querying DAOs: ', e)
-//     throw(e)
-//   }
-//   return '\n'.join(response)
-// }
 
 module.exports = {
   updateDaos,
