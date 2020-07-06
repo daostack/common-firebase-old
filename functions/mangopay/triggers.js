4eae3d0a646fd787f9adafa93cc9863df17ac962
Anton <anton@Antons-Mac-mini.local>
2020-07-06 15:24:53 +0300

    update logic

--- functions/mangopay/triggers.js
+++ functions/mangopay/triggers.js
@@ -1,11 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const { payToDAOStackWallet } = require('../mangopay/mangopay');
 
 exports.watchForExecutedProposals = functions.firestore
   .document('/proposals/{id}')
-  .onUpdate((change) => {
+  .onUpdate(async (change) => {
       const data = change.after.data();
       const previousData = change.before.data();
       if (data.executed !== previousData.executed && data.winningOutcome === 1) {
-          console.log('YES WE DO HAVE TO TRIGGER');
+          console.log('YES WE DO HAVE TO TRIGGER, AND WILL DO IT');
+            const userData = await admin.firestore().collection('users').where("safeAddress", '==', data.proposerId).get().then(doc => { return doc.data() });
+            payToDAOStackWallet({preAuthId: data.preAuthId, Amount: data.joinAndQuit.funding, userData })
       }
   });
