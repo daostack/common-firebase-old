bec4e852269b06a89966b7f3ab582469bf30029d
Anton <anton@Antons-Mac-mini.local>
2020-07-06 19:24:42 +0300

    update trigger function

--- functions/mangopay/triggers.js
+++ functions/mangopay/triggers.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const { payToDAOStackWallet } = require('../mangopay/mangopay');
+const { payToDAOStackWallet, cancelPreauthorizedPayment } = require('../mangopay/mangopay');
 
 exports.watchForExecutedProposals = functions.firestore
   .document('/proposals/{id}')
@@ -8,8 +8,13 @@ exports.watchForExecutedProposals = functions.firestore
       const data = change.after.data();
       const previousData = change.before.data();
       if (data.executed !== previousData.executed && data.winningOutcome === 1) {
-          console.log('YES WE DO HAVE TO TRIGGER, AND WILL DO IT');
-            const userData = await admin.firestore().collection('users').where("safeAddress", '==', data.proposerId).get().then(doc => { return doc.data() });
-            payToDAOStackWallet({preAuthId: data.preAuthId, Amount: data.joinAndQuit.funding, userData })
+          console.log('Proposal EXECUTED and WINNING OUTCOME IS 1 -> INITIATING PAYMENT');
+            const userData = await admin.firestore().collection('users').doc(data.proposerId).get().then(doc => { return doc.data() });
+            const {Status} = payToDAOStackWallet({preAuthId: data.preAuthId, Amount: data.joinAndQuit.funding, userData });
+            return change.after.ref.set({
+              paymentStatus: Status
+            }, {merge: true});
+      } else if (data.executed !== previousData.executed && data.winningOutcome === 0) {
+          cancelPreauthorizedPayment(data.preAuthId);
       }
   });
