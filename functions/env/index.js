f0237f07c58bf85bad146cb3f38b9457a75fe3c2
lpetkov-sw <l.petkov@shopdow.com>
2020-07-08 13:28:52 +0300

    Added deepmerge module

--- functions/env/index.js
+++ functions/env/index.js
@@ -1,6 +1,7 @@
+const merge = require('deepmerge');
 const envSecrets = require('./env_secrets.json');
 const envConfig = require('./env_config.json');
 
-const env = {...envConfig, ...envSecrets};
+const env = merge(envSecrets, envConfig);
 
 exports.env = env;
