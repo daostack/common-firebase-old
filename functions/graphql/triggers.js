b57a24029fe691cad78f98856e9405039e4d25b0
Anton <anton@Antons-Mac-mini.local>
2020-07-16 15:39:25 +0300

    add trigger updating dao on rep redeemed

---
+++ functions/graphql/triggers.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const { updateDaoById } = require('./ArcListener');
+
+exports.watchForReputationRedeemed = functions.firestore
+    .document('/proposals/{id}')
+    .onUpdate(async (change) => {
+        const data = change.after.data();
+        const previousData = change.before.data();
+        if (
+            data.type === 'JoinAndQuit' &&
+            previousData.reputationMinted === "0" && data.reputationMinted !== "0"
+        ) {
+            console.log(
+                'JoinAndQuit proposal reputationMinted changed from "0" Initiating DAO update'
+            );
+            try {
+                updateDaoById(data.dao);
+            } catch (e) {
+                console.log(e);
+            }
+        } 
+    });
