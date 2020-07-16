09d4c276794c6250831bf9f2c1853b73c0feac50
Anton <anton@Antons-Mac-mini.local>
2020-07-16 17:06:41 +0300

    add trigger

--- functions/graphql/triggers.js
+++ functions/graphql/triggers.js
@@ -7,16 +7,17 @@ exports.watchForReputationRedeemed = functions.firestore
         const data = change.after.data();
         const previousData = change.before.data();
         if (
-            data.type === 'JoinAndQuit' &&
-            previousData.reputationMinted === "0" && data.reputationMinted !== "0"
+          data.type === 'JoinAndQuit' &&
+          previousData.joinAndQuit.reputationMinted === '0' &&
+          data.joinAndQuit.reputationMinted !== '0'
         ) {
-            console.log(
-                'JoinAndQuit proposal reputationMinted changed from "0" Initiating DAO update'
-            );
-            try {
-                updateDaoById(data.dao);
-            } catch (e) {
-                console.log(e);
-            }
+          console.log(
+            'JoinAndQuit proposal reputationMinted changed from "0" Initiating DAO update'
+          );
+          try {
+            await updateDaoById(data.dao);
+          } catch (e) {
+            console.log(e);
+          }
         } 
     });
