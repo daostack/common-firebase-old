908ca581d61b6d33271b932ce2193dc6612187e6
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-08-13 13:59:52 +0300

    Use email template

--- functions/mangopay/getPreAuthStatus.js
+++ functions/mangopay/getPreAuthStatus.js
@@ -2,7 +2,9 @@ const { env } = require('../env');
 const {
   viewPreauthorization,
 } = require('./mangopay');
-const { sendMail, MAIL_SUBJECTS } = require('../mailer');
+
+const db = require('firebase-admin').firestore();
+const emailClient = require('../email');
 
 const getPreAuthStatus = async (req) => {
   // eslint-disable-next-line no-useless-catch
@@ -10,7 +12,41 @@ const getPreAuthStatus = async (req) => {
     const { preAuthId } = req.body;
     const { Status, DebitedFunds: { Amount }, AuthorId } = await viewPreauthorization(preAuthId);
     if (Status === 'FAILED') { // this is 3d Authentitacion FAILURE
-      sendMail(env.mail.adminMail, MAIL_SUBJECTS.PREAUTH_FAIL, `3D authentication failed for ${Amount}$ for user with mangopayId ${AuthorId}`);
+      const proposalsSnapshot = await db.collection('proposals')
+        .where('description.preAuthId', '==', preAuthId)
+        .get()
+
+      const proposal = proposalsSnapshot.docs.map(doc => doc.data())[0];
+
+      if(!proposal) {
+        throw new Error("Proposal not found for preauth id " + preAuthId);
+      }
+
+      const common = (await db.collection('daos')
+        .doc(proposal.dao)
+        .get())
+        .data();
+
+      const requester = (await db.collection('users')
+        .doc(proposal.joinAndQuit.proposedMemberId)
+        .get())
+        .data()
+
+      await emailClient.sendTemplatedEmail({
+        to,
+        templateKey: "adminPreauthorizationFailed",
+        emailStubs: {
+          commonName: common.name,
+          membershipRequestId: proposal.id, // @todo Ask if this is right
+          userId: requester.uid,
+          userEmail: requester.email,
+          userFullName: requester.displayName,
+          paymentAmount: proposal.joinAndQuit.funding,
+          submittedOn: new Date(proposal.executedAt * 1000).toDateString(),
+          failureReason: 'Unknown'
+        }
+      });
+
     }
     return { message: 'PreauthStatus', Status };
 
