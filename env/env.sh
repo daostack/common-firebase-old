82254649170fe869f90cbac1ca055880c9ca1680
lmcmz <lmcmze@gmail.com>
2020-08-26 18:16:37 +1000

    Update data.sh

--- env/env.sh
+++ env/env.sh
@@ -11,6 +11,13 @@ stagingCONFIG="$CURRENTDIR/env/staging/env_config.json"
 productionCONFIG="$CURRENTDIR/env/production/env_config.json"
 currentCONFIG="$CURRENTDIR/$DIRNAME/env_config.json"
 
+if ! command -v md5sum &> /dev/null
+then
+    echo "$(tput setaf 1)md5sum $(tput sgr0) could not be found"
+    echo "You can use $(tput setaf 2)brew install md5sha1sum $(tput sgr0)"
+    exit
+fi
+
 if [[ $1 == "-stg" ]]; then
   echo "Switching $(tput setaf 2) staging $(tput sgr0) environment ..."
   cp -f "$stagingENV" "$currentENV"
