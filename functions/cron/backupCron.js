9fc74a72dd4580aac2f016aef60dcc546ecd0382
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-09-01 20:39:12 +0300

    Firestore backup cron

--- functions/cron/backupCron.js
+++ functions/cron/backupCron.js
@@ -1,23 +1,14 @@
-const shell = require('shelljs');
 const functions = require('firebase-functions');
-
-const firestore = require('@google-cloud/firestore');
-const client = new firestore.v1.FirestoreAdminClient();
+const backupUtil = require('@util/backup');
 
 exports.backup = functions.pubsub
   .schedule('0 */3 * * *')
-  .onRun(async (context) => {
+  .onRun(async () => {
+    console.time('Backup');
     console.info('🚀 Beginning backup procedure');
 
-    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-    const databaseName = client.databasePath(projectId, '(default)');
-    const bucket = `gs://common-daostack.appspot.com/backup/${context.timestamp}`;
-
-    await client.exportDocuments({
-      name: databaseName,
-      outputUriPrefix: bucket,
-      collectionIds: []
-    });
+    await backupUtil.backup();
 
     console.info('✨ Backup procedure done successfully');
+    console.timeEnd('Backup');
   });
--- functions/tests/index.js
+++ functions/tests/index.js
@@ -49,44 +49,9 @@ app.get('/sendPreauthFailedEmails', async (req, res) => {
   });
 });
 
-// app.get('/sendMakeProposalsEmails', async (req, res) => {
-//   await processReq(req, res, async () => {
-//     return await testEmailProposalsEmails(req);
-//   })
-// });
-
 app.get('/backup', async (req, res) => {
-  const firestore = require('@google-cloud/firestore');
-  const dateformat = require('dateformat');
-
-  const client = new firestore.v1.FirestoreAdminClient();
-
-  console.info('🚀 Beginning backup procedure');
-
-  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-  const databaseName = client.databasePath(projectId, '(default)');
-
-  const timestamp = dateformat(Date.now(), 'isoDateTime')
-  const bucket =
-    process.env.GCLOUD_PROJECT === 'common-staging-50741'
-      ? `gs://common-staging-50741.appspot.com/backup/${timestamp}` :
-    projectId.env.GCLOUD_PROJECT === 'common-daostack'
-      && `gs://common-daostack.appspot.com/backup/${timestamp}`;
-
-  if(!bucket) {
-    throw new Error('Environment Error: cannot find the current GCloud project!');
-  }
-
-  const result = await client.exportDocuments({
-    name: databaseName,
-    outputUriPrefix: bucket,
-    collectionIds: []
-  });
-
-  console.info('✨ Backup procedure done successfully');
-
-  // res.send("result");
-});
+  res.send(await require('@util/backup').backup())
+})
 
 exports.tests = functions
   .runWith(runtimeOptions)
