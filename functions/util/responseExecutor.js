949bddd1e98176a4b9e8f909c3b19102cd1e471a
jelle <jellegerbrandy@gmail.com>
2020-09-24 15:02:21 +0200

    fix spaces

--- functions/util/responseExecutor.js
+++ functions/util/responseExecutor.js
@@ -10,17 +10,17 @@ const responseExecutor = async (action, { req, res, successMessage, errorMessage
     try {
         const actionResult = await action();
         console.log(actionResult);
-        
+
         res.status(HTTP_STATUS_CODE.OK)
-        .json(
-            {
-                message: successMessage,
-                ...actionResult
-            }
+            .json(
+                {
+                    message: successMessage,
+                    ...actionResult
+                }
             );
-        } catch (e) {
-            console.error(e)
-            res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
+    } catch (e) {
+        console.error(e)
+        res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
             .json(
                 {
                     error: {
@@ -30,46 +30,45 @@ const responseExecutor = async (action, { req, res, successMessage, errorMessage
                     },
                     query: req.query
                 });
-            }
+    }
+}
+
+let retried = false;
+const responseCreateExecutor = async (action, { req, res, successMessage, errorMessage }) => {
+    try {
+        const actionResult = await action();
+        console.log(actionResult)
+        res.status(HTTP_STATUS_CODE.OK)
+            .json(
+                {
+                    message: successMessage,
+                    ...actionResult
+                }
+            );
+    } catch (e) {
+        if ( e.message.match('^No contract with address') && !retried ) {
+            retried = true;
+            await arc.fetchAllContracts(false);
+            console.log('<--- No contract with address error, updating new arc --->');
+            await responseCreateExecutor(action, { req, res, successMessage, errorMessage });
+            return;
         }
         
-        let retried = false;
-        const responseCreateExecutor = async (action, { req, res, successMessage, errorMessage }) => {
-            try {
-                const actionResult = await action();
-                console.log(actionResult)
-                res.status(HTTP_STATUS_CODE.OK)
-                .json(
-                    {
-                        message: successMessage,
-                        ...actionResult
-                    }
-                    );
-                } catch (e) {
-                    if ( e.message.match('^No contract with address') && !retried ) {
-                        retried = true;
-                        await arc.fetchAllContracts(false);
-                        console.log('<--- No contract with address error, updating new arc --->');
-                        await responseCreateExecutor(action, { req, res, successMessage, errorMessage });
-                        return;
-                    }
-                    
-                    console.error("Error ->", e)
-                    res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
-                    .json(
-                        {
-                            error: {
-                                commonMessage: errorMessage,
-                                errorObject: JSON.stringify(e, null, 4),
-                                error: e.toString()
-                            },
-                            query: req.query
-                        });
-                    }
-                }
-                
-                module.exports = {
-                    responseExecutor,
-                    responseCreateExecutor
-                };
-                
+        console.error(e)
+        res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
+            .json(
+                {
+                    error: {
+                        commonMessage: errorMessage,
+                        errorObject: JSON.stringify(e, null, 4),
+                        error: e.toString()
+                    },
+                    query: req.query
+                });
+    }
+}
+
+module.exports = {
+    responseExecutor,
+    responseCreateExecutor
+};
