e709851da94bd979861fe6c3f7ce9b411e276efc
jelle <jellegerbrandy@gmail.com>
2020-09-24 14:46:08 +0200

    logging, indents

--- functions/util/responseExecutor.js
+++ functions/util/responseExecutor.js
@@ -7,55 +7,20 @@ const HTTP_STATUS_CODE = {
 }
 
 const responseExecutor = async (action, { req, res, successMessage, errorMessage }) => {
-   try {
-        const actionResult = await action();
-        console.log(actionResult);
-
-        res.status(HTTP_STATUS_CODE.OK)
-            .json(
-                {
-                    message: successMessage,
-                    ...actionResult
-                }
-            );
-    } catch (e) {
-        console.error(e)
-        res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
-            .json(
-                {
-                    error: {
-                        commonMessage: errorMessage,
-                        errorObject: JSON.stringify(e, null, 4),
-                        error: e.toString()
-                    },
-                    query: req.query
-                });
-    }
-}
-
-let retried = false;
-const responseCreateExecutor = async (action, { req, res, successMessage, errorMessage }) => {
     try {
         const actionResult = await action();
-        console.log(actionResult)
-        res.status(HTTP_STATUS_CODE.OK)
-            .json(
-                {
-                    message: successMessage,
-                    ...actionResult
-                }
-            );
-    } catch (e) {
-        if ( e.message.match('^No contract with address') && !retried ) {
-            retried = true;
-            await arc.fetchAllContracts(false);
-            console.log('<--- No contract with address error, updating new arc --->');
-            await responseCreateExecutor(action, { req, res, successMessage, errorMessage });
-            return;
-        }
+        console.log(actionResult);
         
-        console.error("Error ->", e)
-        res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
+        res.status(HTTP_STATUS_CODE.OK)
+        .json(
+            {
+                message: successMessage,
+                ...actionResult
+            }
+            );
+        } catch (e) {
+            console.error(e)
+            res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
             .json(
                 {
                     error: {
@@ -65,10 +30,46 @@ const responseCreateExecutor = async (action, { req, res, successMessage, errorM
                     },
                     query: req.query
                 });
-    }
-}
-
-module.exports = {
-    responseExecutor,
-    responseCreateExecutor
-};
+            }
+        }
+        
+        let retried = false;
+        const responseCreateExecutor = async (action, { req, res, successMessage, errorMessage }) => {
+            try {
+                const actionResult = await action();
+                console.log(actionResult)
+                res.status(HTTP_STATUS_CODE.OK)
+                .json(
+                    {
+                        message: successMessage,
+                        ...actionResult
+                    }
+                    );
+                } catch (e) {
+                    if ( e.message.match('^No contract with address') && !retried ) {
+                        retried = true;
+                        await arc.fetchAllContracts(false);
+                        console.log('<--- No contract with address error, updating new arc --->');
+                        await responseCreateExecutor(action, { req, res, successMessage, errorMessage });
+                        return;
+                    }
+                    
+                    console.error("Error ->", e)
+                    res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
+                    .json(
+                        {
+                            error: {
+                                commonMessage: errorMessage,
+                                errorObject: JSON.stringify(e, null, 4),
+                                error: e.toString()
+                            },
+                            query: req.query
+                        });
+                    }
+                }
+                
+                module.exports = {
+                    responseExecutor,
+                    responseCreateExecutor
+                };
+                
