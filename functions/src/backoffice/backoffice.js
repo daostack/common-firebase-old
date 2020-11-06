9b0ffc1b8004026434300548cdf89e1780ffd439
geckoslair <enrico@piramid.studio>
2020-11-06 17:43:44 +0100

    correct where clause

--- functions/src/backoffice/backoffice.js
+++ functions/src/backoffice/backoffice.js
@@ -1,8 +1,11 @@
 const { db } = require('../settings.js');
-const COLLECTION_NAME = 'payments';
+const COLLECTION_NAME = 'proposals';
 
 async function getPayout() {
-    const data = await db.collection(COLLECTION_NAME).get();
+    const data = await db.collection(COLLECTION_NAME)
+    .where("winningOutcome", "==", 1)
+    .where("type", "==", "FundingRequest")
+    .get();
     return data.docs.map(doc => doc.data())
 
 }
