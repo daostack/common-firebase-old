1e249e778ee39a12eaaae6de3fb0ea264465ee8f
jelle <jellegerbrandy@gmail.com>
2020-11-12 17:49:58 +0100

    error message

--- functions/src/util/externalRequestExecutor.ts
+++ functions/src/util/externalRequestExecutor.ts
@@ -16,7 +16,9 @@ export const externalRequestExecutor = async (func: () => any, data: IExternalEr
     console.log('External request made successfully');
 
     return result;
-  } catch (e) {
+  } catch (err) {
+    console.error(err.toString(), err)
+    // @todo Please also bulble up the original error to be handled later or shown to the user 
     throw new CommonError(
       data.message || `External service failed. ErrorCode: ${data.errorCode}`,
       data.userMessage || `A call to an external service failed. Please try again later`,
