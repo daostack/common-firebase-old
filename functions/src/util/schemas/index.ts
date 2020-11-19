236814f7419acff1b328d018197665e99af66b59
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-11-19 10:38:45 +0200

    Small tweak (#236)
    * Oooopss
    
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>
    
    * Fix EsLint

--- functions/src/util/schemas/index.ts
+++ functions/src/util/schemas/index.ts
@@ -4,7 +4,7 @@ export const linkValidationSchema = yup.object({
   title: yup.string()
     .max(64),
 
-  address: yup.string()
+  value: yup.string()
     .required()
     .url()
 });
@@ -18,17 +18,6 @@ export const commonRuleValidationSchema = yup.object({
     .string()
     .max(512)
 });
-
-export const commonLinkValidationScheme = yup.object({
-  title: yup
-    .string()
-    .required(),
-
-  value: yup
-    .string()
-    .url()
-    .required()
-})
 
 export const fileValidationSchema = yup.object({
   value: yup
