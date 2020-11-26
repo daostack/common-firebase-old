8bb1079457033d3c15570bed372c57e8b982843c
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-11-26 19:33:41 +0200

    WIP
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

---
+++ functions/src/circlepay/payments/database/index.ts
@@ -0,0 +1,28 @@
+import { db } from '../../../util';
+import { Collections } from '../../../constants';
+
+import { IPaymentEntity } from '../types';
+import { NotImplementedError } from '../../../util/errors';
+
+export const PaymentsCollection = db.collection(Collections.Payments)
+  .withConverter<IPaymentEntity>({
+    fromFirestore(snapshot: FirebaseFirestore.QueryDocumentSnapshot): IPaymentEntity {
+      return snapshot.data() as IPaymentEntity;
+    },
+
+    toFirestore(object: IPaymentEntity | Partial<IPaymentEntity>, options?: FirebaseFirestore.SetOptions): FirebaseFirestore.DocumentData {
+      return object;
+    }
+  });
+
+export const paymentDb = {
+  get: () => {
+    throw new NotImplementedError('Payment Get is not implemented');
+  },
+  getMany: () => {
+    throw new NotImplementedError('Payment GetMany is not implemented');
+  },
+  add: () => {
+    throw new NotImplementedError('Payment Add is not implemented');
+  }
+};
