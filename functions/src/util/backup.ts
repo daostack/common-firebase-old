fa3bbdfd37fa652b8029ec887e87c9d976d99905
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-12-08 10:51:48 +0200

    Baiscally nothing

---
+++ functions/src/util/backup.ts
@@ -0,0 +1,30 @@
+import moment from 'moment';
+import { firestore } from 'firebase-admin';
+import { env } from '../constants';
+import { firebaseAdmin } from '../settings';
+
+const client = new firestore.v1.FirestoreAdminClient();
+
+
+/**
+ * Util for creating a firestore backup for the current
+ * firebase project
+ *
+ * @returns - the result of the backup
+ */
+export const backup = (): Promise<any> => {
+  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+  const databaseName = client.databasePath(projectId, '(default)');
+
+
+  const date = moment(new Date());
+
+  const bucketPrefix = `${env.storage.bucket}/backup/daily/${date.format()}`;
+
+
+  return client.exportDocuments({
+    name: databaseName,
+    outputUriPrefix: bucketPrefix,
+    collectionIds: []
+  });
+};
