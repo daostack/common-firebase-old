52bda972b7ae570c2e816fcac0b010b8ad135bd4
watchtower314 <moore3.14159@gmail.com>
2020-12-23 02:34:54 -0500

    fix double adding user to common

--- functions/src/proposals/triggers/onRequestToJoinExecuted.ts
+++ functions/src/proposals/triggers/onRequestToJoinExecuted.ts
@@ -1,36 +1,37 @@
-  import * as functions from 'firebase-functions';
-  import { Collections } from '../../constants';
-  import { IJoinRequestProposal } from '../proposalTypes';
-  import { createEvent } from '../../util/db/eventDbService';
-  import { EVENT_TYPES } from '../../event/event';
-  import { commonDb } from '../../common/database';
-  import { addCommonMemberByProposalId } from '../../common/business/addCommonMember';
+import * as functions from 'firebase-functions';
+import { Collections } from '../../constants';
+import { IJoinRequestProposal } from '../proposalTypes';
+import { createEvent } from '../../util/db/eventDbService';
+import { EVENT_TYPES } from '../../event/event';
+import { commonDb } from '../../common/database';
+import { addCommonMemberByProposalId } from '../../common/business/addCommonMember';
 
-  export const onRequestToJoinExecuted = functions.firestore
-    .document(`/${Collections.Proposals}/{id}`)
-    .onUpdate(async (proposalSnap) => {
-      const proposal = proposalSnap.after.data() as IJoinRequestProposal;
+export const onRequestToJoinExecuted = functions.firestore
+  .document(`/${Collections.Proposals}/{id}`)
+  .onUpdate(async (proposalSnap) => {
+    const proposal = proposalSnap.after.data() as IJoinRequestProposal;
 
-      if (proposal.paymentState === 'confirmed') {
-        logger.info('Payment was successful. Adding new members to common');
-        
-        // notify user she's now a member
-        await createEvent({
-          type: EVENT_TYPES.REQUEST_TO_JOIN_EXECUTED,
-          userId: proposal.proposerId,
-          objectId: proposal.id
-        });
+    if (proposal.paymentState === 'confirmed'
+      && proposal.join.fundingType === 'one-time') {
+      logger.info('Payment was successful. Adding new members to common');
 
-        // Update common funding info
-        const common = await commonDb.getCommon(proposal.commonId);
+      // notify user she's now a member
+      await createEvent({
+        type: EVENT_TYPES.REQUEST_TO_JOIN_EXECUTED,
+        userId: proposal.proposerId,
+        objectId: proposal.id
+      });
 
-        common.raised += proposal.join.funding;
-        common.balance += proposal.join.funding;
+      // Update common funding info
+      const common = await commonDb.getCommon(proposal.commonId);
 
-        await commonDb.updateCommon(common);
+      common.raised += proposal.join.funding;
+      common.balance += proposal.join.funding;
 
-        // Add the user as member
-        await addCommonMemberByProposalId(proposal.id);
-      }
+      await commonDb.updateCommon(common);
+
+      // Add the user as member
+      await addCommonMemberByProposalId(proposal.id);
     }
-  );
+  }
+);
