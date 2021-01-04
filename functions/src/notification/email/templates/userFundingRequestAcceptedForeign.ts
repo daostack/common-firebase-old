2ed89f65529306817be83559a662ce87fe30aaf5
Taikaish <taikaish@gmail.com>
2021-01-04 14:05:26 +0200

    Update userFundingRequestAcceptedForeign.ts
    Linebreaks and deleting a duplicated line

--- functions/src/notification/email/templates/userFundingRequestAcceptedForeign.ts
+++ functions/src/notification/email/templates/userFundingRequestAcceptedForeign.ts
@@ -2,13 +2,13 @@ const template = `
 Hello {{userName}},
 <br /><br />
 Congratulations! <br />
-Your proposal {{proposal}} to {{commonName}} has been approved. To receive the funds ({{fundingAmount}}) you will need to provide your bank account details, as well as some identification information.
-We will reach out soon with further instructions on how to submit those details.
+Your proposal {{proposal}} to {{commonName}} has been approved. To receive the funds ({{fundingAmount}}) you will need to provide your bank account details, as well as some identification information.<br /><br />
+We will reach out soon with further instructions on how to submit those details.<br />
 When submitted we will be able to initiate the transaction. It may take up to 10 days for the funds to be transfered. 
-<br /><br /><br /><br />
+<br /><br />
 Please note that funds are sent via an international bank wire. Your bank might charge fees for receipt of wires, currency conversion or other applicable fees. Due to those fees, the amount you’ll receive may be lower than the original requested amount.
+<br /><br />
 To ensure transparency, after receiving the funds and paying for the proposed products or services, you will be required to submit Invoices and/or receipts for the payments made.
-For more information you can contact us any time by replying to this email.
 <br /><br />
 For more information you can contact us any time by replying to this email.
 <br /><br />
