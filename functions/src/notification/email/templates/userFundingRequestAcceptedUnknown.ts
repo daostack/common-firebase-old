33edb80c63844c66efcc29ad8e32a145912bc759
watchtower314 <moore3.14159@gmail.com>
2021-01-04 08:23:41 -0500

    added spacing and line breaks

--- functions/src/notification/email/templates/userFundingRequestAcceptedUnknown.ts
+++ functions/src/notification/email/templates/userFundingRequestAcceptedUnknown.ts
@@ -1,17 +1,20 @@
 const template = `
 Hello {{userName}},
 <br /><br />
-Congratulations! <br />
+Congratulations!
+<br /><br />
 Your proposal "{{proposal}}" to {{commonName}} has been approved. To send you the funds ({{fundingAmount}}), we first need you to complete some missing information.
-<br /><br /><br /><br />
+<br /><br />
 Please include the following details in a return email:
+<br /><br />
 Street address, town/city, state, country, zip code.
 <br /><br />
 Once received, we will send you another email with instructions on how to proceed and get the funds.
 <br /><br />
 For more information you can contact us any time by replying to this email.
 <br /><br />
-Common,<br />
+Common,
+<br /><br />
 Collaborative Social Action.
 `;
 
