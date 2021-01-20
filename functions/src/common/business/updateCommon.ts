1890610d667f70f543099deab53ecc2d859b0181
watchtower314 <moore3.14159@gmail.com>
2021-01-20 07:47:46 -0500

    wip

--- functions/src/common/business/updateCommon.ts
+++ functions/src/common/business/updateCommon.ts
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
 
+import { CommonError } from '../../util/errors';
 import { commonDb } from '../database';
 import { ICommonUpdate, ICommonEntity } from '../types';
 import { createEvent } from '../../util/db/eventDbService';
@@ -8,38 +9,24 @@ import { commonEditHistoryDb } from '../../commonEditHistory/database';
 import { linkValidationSchema, commonRuleValidationSchema } from '../../util/schemas';
 
 const updateCommonDataValidationScheme = yup.object({
-commonId: yup
-.string()
-.required(),  newCommon: yup.object({
+  commonId: yup
+    .string()
+    .required(),  
+
+  userId: yup.string()
+    .required(),
+
+  changes: yup.object({
     name: yup.string().required(),//
     rules: yup.array(commonRuleValidationSchema).optional(),//
-    updatedAt: yup.string().required(), //
-    members: yup.array().required(),//
+    
     metadata: yup.object({
-      action: yup.string(),
-      byline: yup.string().required(),//
-      contributionType: yup
-        .string()
-        .oneOf(['one-time', 'monthly'])
-        .default('one-time')
-        .required().required(),
-      description: yup.string().required(),
-      founderId: yup.string().required(),
-      minFeeToJoin: yup.number().required(),
-    }).required(),
-    balance: yup.number().required(),//
-    raised: yup.number().required(), //
+      byline: yup.string(),
+      description: yup.string(),
+    }),
     image: yup.string().url().required(),//
-    register: yup
-      .string()
-      .oneOf(['na', 'registered'])
-      .default('na')
-      .required(),//
-    links: yup.array(linkValidationSchema).optional(),//
-    createdAt: yup.string().required(),//   
-    fundingGoalDeadline: yup.number().required(), //
   }).required(),
-  changedBy: yup.string().required(),
+ 
 })
 
 type UpdateCommonPayload = yup.InferType<typeof updateCommonDataValidationScheme>
@@ -49,8 +36,12 @@ type UpdateCommonPayload = yup.InferType<typeof updateCommonDataValidationScheme
  * @param commonUpdate       - info of the common that needs to be updated
  * @return updatedCommon     - the common doc after the update
  */
-export const updateCommon = async (commonUpdate: UpdateCommonPayload) : Promise<ICommonEntity> => {
-  const common = await commonDb.get(commonUpdate.newCommon.id);
+export const updateCommon = async (payload: UpdateCommonPayload) : Promise<ICommonEntity> => {
+  const common = await commonDb.get(payload.commonId);
+
+  if (common.metadata.founderId !== payload.userId) {
+       throw new CommonError('Try again when you created the common')
+  }
 
   console.log('========= commonUpdate struct: =========== ', commonUpdate)
   // the doc that was saved in the commonEditHistory collection
